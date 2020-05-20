3b096b2b95acd4bb351dbb4883949b09d0d7cc87
zaidrazakhan <zaid.kv5@gmail.com>
2020-05-21 05:10:00 +0530

    Reconfigured options,py to make it running on gpu

--- src/options.py
+++ src/options.py
@@ -23,8 +23,8 @@ class Options():
         parser.add_argument('--grl_lambda', type=float, default=0.5, help='Lambda used to normalize the GRL layer.')
         parser.add_argument('--nopretrain', action='store_false', help='Loads a pretrained model (Default: True).')
         # Optimization options
-        parser.add_argument('--epochs', '-e', type=int, default=1000, help='Number of epochs to train.')
-        parser.add_argument('--batch_size', '-b', type=int, default=20, help='Batch size.')
+        parser.add_argument('--epochs', '-e', type=int, default=100, help='Number of epochs to train.')
+        parser.add_argument('--batch_size', '-b', type=int, default=10, help='Batch size.')
         parser.add_argument('--learning_rate', '-lr', type=float, default=1e-4, help='The Learning Rate.')
         parser.add_argument('--momentum', '-m', type=float, default=0.9, help='Momentum.')
         parser.add_argument('--decay', '-d', type=float, default=0.0005, help='Weight decay (L2 penalty).')
@@ -35,14 +35,14 @@ class Options():
         parser.add_argument('--w_triplet', type=float, default=1, help='Triplet loss Weight.')
         parser.add_argument('--gamma', type=float, default=0.1, help='LR is multiplied by gamma on schedule.')
         parser.add_argument('--seed', type=int, default=42, help='Random seed.')
-        parser.add_argument('--save', '-s', type=str, default=None, help='Folder to save checkpoints.')
-        parser.add_argument('--load', '-l', type=str, default=None, help='Checkpoint path to resume / test.')
-        parser.add_argument('--early_stop', '-es', type=int, default=20, help='Early stopping epochs.')
+        parser.add_argument('--save', '-s', type=str, default="./", help='Folder to save checkpoints.')
+        parser.add_argument('--load', '-l', type=str, default="/DATA/khan.2/logs/29_run-batchSize_10/checkpoint.pth", help='Checkpoint path to resume / test.')
+        parser.add_argument('--early_stop', '-es', type=int, default=10, help='Early stopping epochs.')
         # Acceleration
         parser.add_argument('--ngpu', type=int, default=1, help='0 = CPU, 1 = CUDA, 1 < DataParallel')
-        parser.add_argument('--prefetch', type=int, default=0, help='Pre-fetching threads.')
+        parser.add_argument('--prefetch', type=int, default=3, help='Pre-fetching threads.')
         # i/o
-        parser.add_argument('--log', type=str, default=None, help='Log folder.')
+        parser.add_argument('--log', type=str, default='./logs/', help='Log folder.')
         parser.add_argument('--log-interval', type=int, default=20, metavar='N',
                             help='How many batches to wait before logging training status')
         parser.add_argument('--attn', action='store_true', help='Attention module (Default: False).')
@@ -56,4 +56,3 @@ class Options():
 
     def parse(self):
         return self.parser.parse_args()
-
