ac644ccb64c8eb12c19210d3bfef338dcf221489
zaidrazakhan <zaid.kv5@gmail.com>
2020-07-27 03:14:08 +0530

    Updated MSCocoDataLoader

--- src/MSCocoDataloader.py
+++ src/MSCocoDataloader.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import torch
 import glob
+import cv2
+import numpy as np
 
 class MSCocoDataloader(torch.utils.data.Dataset):
     def __init__(self, root_dir='/DATA/dataset/mscoco/'):
@@ -8,10 +10,11 @@ class MSCocoDataloader(torch.utils.data.Dataset):
         self.labels = glob.glob(self.root_dir)
     
     def __len__(self):
-        return len(len(self.labels))
+        return len(self.labels)
 
     def __getitem__(self, idx):
         return Image.open(self.labels[idx])
+        # return Image.open(self.labels[idx])
     
     def get_image_name(self, idx):
         return self.labels[idx]
